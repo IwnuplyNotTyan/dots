d0a5c7f5420e663d479a99c5c61a33813d092a05
Stas Fylypets <78899891+IwnuplyNotTyan@users.noreply.github.com>
2023-02-24 17:46:29 +0000

    github page

---
+++ config.js
@@ -0,0 +1,209 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Stas',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '4',
+			name: 'Twitch',
+			icon: 'twitch',
+			link: 'https://www.twitch.tv/',
+		},
+		{
+			id: '3',
+			name: 'pocket',
+			icon: 'pocket',
+			link: 'https://www.pocket.com',
+		},
+		{
+			id: '2',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '5',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com',
+		},
+		{
+			id: '6',
+			name: 'Odysee',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://www.aliexpress.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'banana',
+			id: '1',
+			links: [
+				{
+					name: 'r/*nixporn',
+					link: 'https://www.reddit.com/r/unixporn/',
+				},
+				{
+					name: 'r/rainmiter',
+					link: 'https://www.reddit.com/r/Rainmeter/',
+				},
+				{
+					name: 'r/wallpaper',
+					link: 'https://www.reddit.com/r/wallpaper/',
+				},
+				{
+					name: 'r/cats',
+					link: 'https://www.reddit.com/r/cats/',
+				},
+			],
+		},
+		{
+			icon: 'coffee',
+			id: '2',
+			links: [
+				{
+					name: 'Custom',
+					link: 'https://drive.google.com/file/d/1k1pexrSiGqqtqvvjhIgv52QiBiK4fnMp/view?usp=sharing',
+				},
+				{
+					name: 'All Links',
+					link: 'https://drive.google.com/folderview?id=1QBKq6wkjoHHY5yxHEp2E-gGjJ1GeNJtB&usp=sharing',
+				},
+				{
+					name: 'Wallpaper',
+					link: 'https://drive.google.com/folderview?id=1D7BUl8HZSTFspG59H9691TWWGFxup_Pa&usp=sharing',
+				},
+				{
+					name: 'Othere',
+					link: 'https://drive.google.com/file/d/1Olb4NkY3sAfMu1Gqtw_U28-wiblKI7Um/view?usp=sharing',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
